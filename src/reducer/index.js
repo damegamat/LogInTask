21a525ce5ded82dc7ad22b97131f7077fad8bb3b
Darek <dar.lewandowski93@gmail.com>
2020-01-17 12:19:26 +0100

    update

---
+++ src/reducer/index.js
@@ -0,0 +1,12 @@
+// import { combineReducers } from "redux";
+
+// export default combineReducers({});
+
+const initState = {
+  raz: []
+};
+const rootReducer = (state = initState, action) => {
+  return state;
+};
+
+export default rootReducer;
--- src/store.js
+++ src/store.js
@@ -1,8 +1,13 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import reducer from "./reducer";
+import thunk from "redux-thunk";
+import rootReducer from "./reducer/index";
 
-const rootReducer = () => {
-  console.log("ok");
-};
-const store = createStore(rootReducer);
+// const rootReducer = () => {
+//   console.log("ok");
+// };
+const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
+
+store.dispatch({ type: "TO", text: "asda" });
 
 export default store;
