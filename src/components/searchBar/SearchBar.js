f968611150b9ff753b4e0e586388da91693abcb6
Darek <dar.lewandowski93@gmail.com>
2020-01-19 14:04:10 +0100

    add SearchBar

---
+++ src/components/searchBar/SearchBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SearchBar.css";
+
+const SearchBar = () => {
+  return (
+    <div className="searchBar">
+      <input />
+    </div>
+  );
+};
+
+export default SearchBar;
