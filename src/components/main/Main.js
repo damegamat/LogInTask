27e49b5cee426cda69be9283290de7eb39ba3824
Darek <dar.lewandowski93@gmail.com>
2020-01-20 23:36:32 +0100

    add fetch and filter users

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import UserList from "../userList/UserList";
+import { connect } from "react-redux";
+import * as usersActions from "../../store";
+import getFilteredUsers from "../../selectors/getFilteredUsers";
 
-const Main = () => {
-  return <UserList />;
+const Main = ({ users, fetchUsers, searchValue }) => {
+  let currentUsers;
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  if (users.length > 0) {
+    currentUsers = getFilteredUsers(users, searchValue);
+  } else {
+    currentUsers = users;
+  }
+
+  return <UserList users={currentUsers} />;
 };
 
-export default Main;
+const mapStateToProps = state => {
+  return {
+    users: state.users.users,
+    searchValue: state.searchValue.searchValue
+  };
+};
+
+export default connect(mapStateToProps, usersActions)(Main);
