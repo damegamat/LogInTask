d09c1e1ce725ba976caeb6d8b7532bdccddc1fc3
Darek <dar.lewandowski93@gmail.com>
2020-01-20 23:38:19 +0100

    update form

--- src/components/signin/SignIn.js
+++ src/components/signin/SignIn.js
@@ -2,32 +2,30 @@ import React, { useState, useEffect } from "react";
 import { Redirect, Route } from "react-router-dom";
 import "./SignIn.css";
 function SignIn() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [form, setForm] = useState({ username: "", password: "" });
 
   const handleChange = e => {
-    const data = e.target.name;
-    if (data === "username") {
-      setUsername(e.target.value);
-    } else if (data === "password") {
-      setPassword(e.target.value);
-    }
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    });
   };
 
   const handleSubmitForm = e => {
     e.preventDefault();
+    console.log(form);
   };
 
   return (
     <div className="loginContent">
-      <h1>SignIn</h1>
+      <h1>Sign In</h1>
       <form onSubmit={e => handleSubmitForm(e)}>
         <input
           type="text"
           placeholder="username"
           name="username"
           id="username"
-          value={username}
+          value={form.username}
           onChange={e => handleChange(e)}
         />
 
@@ -35,8 +33,8 @@ function SignIn() {
           type="password"
           placeholder="password"
           name="password"
-          id="uassword"
-          value={password}
+          id="password"
+          value={form.password}
           onChange={e => handleChange(e)}
         />
 
