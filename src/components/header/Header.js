267a01ce583e6b4ec8b618ee81c84dd9ff0e780f
Darek <dar.lewandowski93@gmail.com>
2020-01-20 23:35:25 +0100

    add navlink

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,11 +2,14 @@ import React from "react";
 import Btn from "../btn/Btn";
 import SearchBar from "../searchBar/SearchBar";
 import "./Header.css";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="header">
-      <Btn />
+      <NavLink to="/signin">
+        <Btn text={`logout`} />
+      </NavLink>
       <SearchBar />
     </div>
   );
