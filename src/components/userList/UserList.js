33119688b9aa2a76cd19d339ce732b63837a7653
Darek <dar.lewandowski93@gmail.com>
2020-01-20 23:40:49 +0100

    map users

--- src/components/userList/UserList.js
+++ src/components/userList/UserList.js
@@ -1,16 +1,14 @@
 import React from "react";
 import "./UserList.css";
 import UserSummary from "../userSummary/UserSummary";
-const UserList = () => {
-  return (
-    <div className="userList">
-      <UserSummary />
-      <UserSummary />
-      <UserSummary />
-      <UserSummary />
-      <UserSummary />
-    </div>
-  );
+const UserList = ({ users }) => {
+  const user =
+    users &&
+    users.map(user => {
+      return <UserSummary key={user.id} user={user} />;
+    });
+
+  return <div className="userList">{user}</div>;
 };
 
 export default UserList;
