237f72f49ec2c070077af45dd2e7bd2a66dfa013
Darek <dar.lewandowski93@gmail.com>
2020-01-23 19:38:55 +0100

    add switch

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,7 +1,19 @@
-const initState = {};
+import { AUTH } from "../actions/types";
+
+const initState = false;
 
 const authReducer = (state = initState, action) => {
-  return state;
+  // console.log(action);
+  switch (action.type) {
+    case AUTH:
+      return {
+        ...state,
+        auth: action.auth
+      };
+
+    default:
+      return state;
+  }
 };
 
 export default authReducer;
