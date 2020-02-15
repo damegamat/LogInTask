7a53fce9b0743e788560b147ee0b744b2edbca81
Darek <dar.lewandowski93@gmail.com>
2020-02-15 23:12:01 +0100

    add login reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,11 +1,13 @@
 import authReducer from "./authReducer";
 import userReducer from "./userReducer";
 import searchReducer from "./searchReducer";
+import loginReducer from "./loginReducer";
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   users: userReducer,
-  searchValue: searchReducer
+  searchValue: searchReducer,
+  login: loginReducer
 });
 export default rootReducer;
