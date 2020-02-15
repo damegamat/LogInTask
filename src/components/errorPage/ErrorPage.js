c6031058f2ad6f66c4246cb7042041f776080a75
Darek <dar.lewandowski93@gmail.com>
2020-02-15 21:13:44 +0100

    create component ErrorPage

---
+++ src/components/errorPage/ErrorPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ErrorPage.css";
+
+const errorPage = () => {
+  return (
+    <div class="container">
+      <h1>sorry, page doesn't exist ;(</h1>
+    </div>
+  );
+};
+
+export default errorPage;
