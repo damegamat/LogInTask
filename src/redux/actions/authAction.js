3be4d7cd8a68d0bdfcf1c3ac254eb76d95311f3d
Darek <dar.lewandowski93@gmail.com>
2020-02-15 23:14:49 +0100

    delete auth file

--- src/redux/actions/authAction.js
+++
@@ -1,9 +0,0 @@
-import { AUTH } from "./types";
-
-export function authAction(auth) {
-  console.log(auth);
-  return {
-    type: AUTH,
-    auth
-  };
-}
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,11 +1,9 @@
-import authReducer from "./authReducer";
 import userReducer from "./userReducer";
 import searchReducer from "./searchReducer";
 import loginReducer from "./loginReducer";
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
-  auth: authReducer,
   users: userReducer,
   searchValue: searchReducer,
   login: loginReducer
