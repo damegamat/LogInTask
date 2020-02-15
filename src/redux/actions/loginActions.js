ed911167e0cde6e74b472b66dbf3d98b029162ea
Darek <dar.lewandowski93@gmail.com>
2020-02-15 23:05:54 +0100

    login actions

---
+++ src/redux/actions/loginActions.js
@@ -0,0 +1,24 @@
+import { SET_LOGIN_PENDING } from "./types";
+import { SET_LOGIN_SUCCESS } from "./types";
+import { SET_LOGIN_ERROR } from "./types";
+
+export function setLoginPending(isLoginPending) {
+  return {
+    type: SET_LOGIN_PENDING,
+    isLoginPending
+  };
+}
+
+export function setLoginSuccess(isLoginSuccess) {
+  return {
+    type: SET_LOGIN_SUCCESS,
+    isLoginSuccess
+  };
+}
+
+export function setLoginError(loginError) {
+  return {
+    type: SET_LOGIN_ERROR,
+    loginError
+  };
+}
